1b3305c1a254fdfc429ccd540d3845b441f0569c
thisri1110@gmail.com <thisri1110@gmail.com>
2025-06-22 11:07:56 +0000

    Commit the stg changes

--- models/tgt_employees.sql
+++ models/tgt_employees.sql
@@ -1,15 +1,23 @@
-select 
-    EMPLOYEE_ID,
-    FIRST_NAME,
-    LAST_NAME,
-    EMAIL,
-    PHONE_NUMBER,
-    HIRE_DATE,
-    JOB_ID,
-    SALARY,
-    COMMISSION_PCT,
-    MANAGER_ID,
-    DEPARTMENT_ID,
-    current_timestamp as load_date
-from HR.employees
- 
+{{ config(materialized= 'incremental',
+            unique_key = 'employee_id') }}
+
+select
+    employee_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    hire_date,
+    job_id,
+    salary,
+    commission_pct,
+    manager_id,
+    department_id,
+    current_timestamp as load_time
+from HR.Employees1
+{% if is_incremental() %}
+
+where load_time >= (
+        select coalesce(max(load_time), '1900-01-01 00:00:00') from {{this}} )
+
+{% endif %}
